da756b3f09290bf1a22ec701cf0234c3dd26cfb0
coastforge-cro <claudio.ross@kuestenschmiede.de>
2017-06-23 15:50:40 +0200

    1.1.2 snapshot

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -25,7 +25,7 @@ class ApiController extends FrontendController
      * @var string
      */
     private $_sApiUrl = 'system/modules/con4gis_core/api/index.php';
-    private static $_sApiBundleVersion = '1.1.1';
+    private static $_sApiBundleVersion = '1.1.2-snapshot';
 
     public function runAction($_url_fragment)
     {
